4814e42c6d79b60f1b7be9d9e2381ac6612d260e
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-05-09 09:41:19 +0000

    working in progress

--- amazon_flask.py
+++ amazon_flask.py
@@ -3,16 +3,11 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+df = pd.read_csv('/workspace/Amazon_Flask/data/prodotti.csv')
+
 @app.route('/')
 def homepage():
-    return render_template('index.html')
-
-@app.route('/elenco')
-def elenco_prodotti():
-    df = pd.read_csv('/workspace/Amazon_Flask/data/prodotti.csv')
-    return render_template('tabella.html', tabella = df.to_html())
-
-
+    return render_template('index.html', prodotti=df.to_dict('records'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
